509232701d26fb92be16e38fbb56a2b2dba02498
samoskip <samueloskipper@gmail.com>
2020-06-17 09:15:38 -0500

    Basic UI fixes

--- blog/views.py
+++ blog/views.py
@@ -54,7 +54,7 @@ def post_publish(request, pk):
     post.publish()
     return redirect('post_detail', pk=pk)
 
-@login_required
+# @login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
